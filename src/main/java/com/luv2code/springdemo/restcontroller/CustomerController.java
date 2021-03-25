d2e9670280c5e9296cf4f5280e572b8463be168c
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-25 14:59:13 +0200

    Adding end point for POST request.

--- src/main/java/com/luv2code/springdemo/restcontroller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/restcontroller/CustomerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +30,22 @@ public class CustomerController {
 	}
 	
 	@GetMapping("/customers/{customerId}")
-	public Customer geCustomer (@PathVariable int customerId) {
+	public Customer getCustomer (@PathVariable int customerId) {
 		
 		return customerService.getCustomer(customerId);
+	}
+	
+	@PostMapping("/customers")
+	public Customer addNewCustomer(@RequestBody Customer newCustomer) {
+		
+		
+		// set id 0 forces a save to the new item instead of update 
+		// we do this in case the user provides id of the customer
+		// the back-end will generate id, there is no use to be set at client level
+		newCustomer.setId(0);
+		
+		customerService.saveCustomer(newCustomer);
+		
+		return newCustomer;
 	}
 }
