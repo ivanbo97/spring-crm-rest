2deeb4b78c48380c70cd80f1cdc6aa9a4fb1672e
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-26 10:32:13 +0200

    Adding authentication based on credentials stored in DB.

--- src/main/java/com/luv2code/springdemo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springdemo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.luv2code.springdemo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,17 +17,13 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource myDataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		// add our users for in memory authentication (for test purposes)
 		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-			.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-			.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
+		auth.jdbcAuthentication().dataSource(myDataSource);
 	}
 	
 	@Override
