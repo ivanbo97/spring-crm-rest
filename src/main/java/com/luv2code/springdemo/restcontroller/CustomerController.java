0d2f6a60243a2b0ce68abf16a678bb5b7d5c4667
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-26 08:52:56 +0200

    Adding end point for DELETE requests.

--- src/main/java/com/luv2code/springdemo/restcontroller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/restcontroller/CustomerController.java
@@ -3,16 +3,20 @@ package com.luv2code.springdemo.restcontroller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.exception.CustomerNotFoundException;
 import com.luv2code.springdemo.service.CustomerService;
+import com.luv2code.springdemo.util.CheckCustomer;
 
 
 
@@ -23,6 +27,8 @@ public class CustomerController {
 	@Autowired
 	private CustomerService customerService;
 	
+	@Autowired
+	private CheckCustomer checkCustomer;
 	
 	@GetMapping("/customers")
 	public List<Customer> getCustomers(){
@@ -32,6 +38,10 @@ public class CustomerController {
 	
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer (@PathVariable int customerId) {
+		
+		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		checkCustomer.existence(customerId);
 		
 		return customerService.getCustomer(customerId);
 	}
@@ -56,6 +66,16 @@ public class CustomerController {
 		customerService.saveCustomer(customer);
 		
 		return customer;
+	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public void deleteCustomer(@PathVariable int customerId) {
+		
+		Customer customer = customerService.getCustomer(customerId);
+		
+		checkCustomer.existence(customerId);
+		
+		customerService.deleteCustomer(customerId);
 	}
 	
 }
